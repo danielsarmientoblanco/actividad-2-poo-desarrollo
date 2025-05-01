18c93fad4c7788afb5e8a7e8bdbaa78d29f7b966
Daniel Sarmiento <kyoshirambeau@gmail.com>
2025-05-01 18:23:28 -0500

    Update EJERCICIO1.py

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -11,7 +11,8 @@ class Persona:
         print(f"Número de documento de identidad = {self.numeroDocumentoIdentidad}")
         print(f"Año de nacimiento = {self.añoNacimiento}")
         print()
-        
+
+
 persona1 = Persona("Pedro", "Pérez", "1053121010", 1998)
 persona2 = Persona("Luis", "León", "1053223344", 2001)
 persona1.MostrarDatos()
