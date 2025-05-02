2e727accd5f7f8e7f6d411bd1362bb6d86f06ae0
Daniel Sarmiento <kyoshirambeau@gmail.com>
2025-05-01 21:59:38 -0500

    Update EJERCICIO1.py

--- EJERCICIO1.py
+++ EJERCICIO1.py
@@ -5,7 +5,7 @@ class Persona:
         self.númeroDocumentoIdentidad= númeroDocumentoIdentidad
         self.añoNacimiento = añoNacimiento        
         
-    def MostrarDatos(self):
+    def mostrarDatos(self):
         print(f"Nombre = {self.nombre}")
         print(f"Apellidos = {self.apellidos}")
         print(f"Número de documento de identidad = {self.númeroDocumentoIdentidad}")
@@ -15,5 +15,5 @@ class Persona:
 
 persona1 = Persona("Pedro", "Pérez", "1053121010", 1998)
 persona2 = Persona("Luis", "León", "1053223344", 2001)
-persona1.MostrarDatos()
-persona2.MostrarDatos()
+persona1.mostrarDatos()
+persona2.mostrarDatos()
